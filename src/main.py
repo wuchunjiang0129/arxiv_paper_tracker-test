63d75bb7b76649646f1f5435e69339176f67fdd1
wuchunjiang <wuchunjiang@neolix.ai>
2026-06-03 12:54:04 +0800

    dependencies

--- src/main.py
+++ src/main.py
@@ -37,8 +37,8 @@ EMAIL_TO = [email.strip() for email in os.getenv("EMAIL_TO", "").split(",") if e
 
 PAPERS_DIR = Path("./papers")
 CONCLUSION_FILE = Path("./conclusion.md")
-CATEGORIES = ["cs.RO", "cs.CV", "cs.AI"]
-MAX_PAPERS = 50  # 设置为1以便快速测试
+CATEGORIES = ["cs.CV", "cs.AI","cs.RO",]
+MAX_PAPERS = 10  # 设置为1以便快速测试
 
 # 配置OpenAI API用于DeepSeek
 # openai.api_key = DEEPSEEK_API_KEY
